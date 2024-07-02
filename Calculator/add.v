ea4c471fd18c26b475798478c157ec5a2f9c1d9b
Jcpaul1226 <155691679+Jcpaul1226@users.noreply.github.com>
2024-07-01 21:17:20 -0700

    Add files via upload

---
+++ Calculator/add.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/19/2024 09:51:35 AM
+// Design Name: 
+// Module Name: add
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module add(
+    input [6:0] x,
+    input [6:0] y,
+    input [6:0] result
+    );
+    
+endmodule
---
+++ Calculator/div.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/19/2024 09:51:35 AM
+// Design Name: 
+// Module Name: div
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module div(
+    input [3:0] x,
+    input [3:0] y,
+    output [3:0] result
+    );
+endmodule
